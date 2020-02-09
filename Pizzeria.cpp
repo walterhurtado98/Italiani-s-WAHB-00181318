a228355a28dd2e2398c0774ae02d26d09fa216a5
walterhurtado98 <walterhurtado1998@gmail.com>
2020-02-09 00:37:55 -0600

    adding new function

--- Pizzeria.cpp
+++ Pizzeria.cpp
@@ -27,16 +27,17 @@ struct customerData{
 	eT enTri;
 	mD maDi;
 	dS drK;
-	
 };
 typedef struct customerData cData;
 
 void menuEmpleado();
 void menuAdmin();
 void delivery();
+void restaurant();
 void viewOrders();
 
 vector<cData> deliveryList;
+vector<cData> restaurantList;
 
 int main(){
 	char option;
@@ -77,7 +78,7 @@ void menuEmpleado(){
 	
 	do{
 		cout << "\n|MENU|" << endl;
-		cout << "\t1. Agregar pedido a domicilio\n\t2. Agregar encargo a restaurant\n\t3. Ver pedidos a domicilio\n\t4. Ver encargos en el restaurante\n\t5. Salir" << endl;
+		cout << "\t1. Agregar pedido a domicilio\n\t2. Agregar encargo a restaurante\n\t3. Ver pedidos a domicilio\n\t4. Ver encargos en el restaurante\n\t5. Salir" << endl;
 		cout << "Su opcion: ";
 		cin >> option;
 		
@@ -103,7 +104,7 @@ void menuAdmin(){
 		
 		switch(option){
 			case 1: delivery(); break;
-			case 2: break;
+			case 2: restaurant();break;
 			case 3: break;
 			case 4: break;
 			case 5: break;
@@ -183,6 +184,78 @@ void delivery(){
 		default: "Escoga una opcion corecta"; 
 	}
 	deliveryList.push_back(p);
+	
+	cout << "\nSu cuenta es: " << "$" << amount <<endl;
+		
+}
+
+void restaurant(){
+	cData p;
+	int option = 0;
+	int option2 = 0;
+	int option3 = 0;
+	int option4 = 0;
+	float amount = 0;
+	
+	cout << "\nIngrese sus datos: " << endl;
+	cin.ignore();
+	cout << "\tNombre completo: ";
+	getline(cin, p.name);
+	cout << "\tDireccion: ";
+	getline(cin, p.direction);
+	cout << "\tTelefono: ";
+	cin >> p.phone;
+	
+	cout << "\n\t1. Entradas\n\t2. Plato principal\n\t3. Bebidas"<<endl;
+	cout << "Su opcion: ";
+	cin >> option;
+	
+	switch(option){
+		case 1: 
+				cout << "\n----------------\n";
+				cout << "    ENTRADAS    \n";
+				cout << ".................";
+				cout << "\n\n1. Rollos de piz\n2. Pan con ajo\n3. Palitos de queso" << endl;
+				cout << "Su opcion: ";
+				cin >> option2;
+				switch(option2){
+					case 1: cout << "\n¿Cuantos desea?: "; cin >> p.enTri.pizzaRolls; amount = amount + (4.99 * p.enTri.pizzaRolls); break;
+					case 2: cout << "\n¿Cuantos desea?: "; cin >> p.enTri.garlicBread; amount = amount + (3.99 * p.enTri.garlicBread); break;
+					case 3: cout << "\n¿Cuantos desea?: "; cin >> p.enTri.cheeseSticks; amount = amount + (3.75 * p.enTri.cheeseSticks); break;
+				}
+				
+		break;
+		case 2: 
+		        cout << "\n-----------------------\n";
+				cout << "   PLATOS PRINCIPALES    \n";
+				cout << "........................";
+				cout << "\n\n1. Pizza\n2. Pasta\n3. Lasagna" << endl;
+				cout << "Su opcion: ";
+				cin >> option3;
+				switch(option3){
+					case 1: cout << "\n¿Cuantos desea?: "; cin >> p.maDi.pizza; amount = amount + (13.99 * p.maDi.pizza); break;
+					case 2: cout << "\n¿Cuantos desea?: "; cin >> p.maDi.pasta; amount = amount + (5.55 * p.maDi.pasta); break;
+					case 3: cout << "\n¿Cuantos desea?: "; cin >> p.maDi.lasagna; amount = amount + (6.25 * p.maDi.lasagna); break;
+				}
+				
+		break;
+		case 3: 
+		        cout << "\n-----------------\n";
+				cout << "     BEBIDAS    \n";
+				cout << "..................";
+				cout << "\n\n1. Cerveza\n2. Soda\n3. Te helado" << endl;
+				cout << "Su opcion: ";
+				cin >> option4;
+				switch(option4){
+					case 1: cout << "\n¿Cuantos desea?: "; cin >> p.drK.beer; amount = amount + (1.99 * p.drK.beer); break;
+					case 2: cout << "\n¿Cuantos desea?: "; cin >> p.drK.soda; amount = amount + (0.95 * p.drK.soda); break;
+					case 3: cout << "\n¿Cuantos desea?: "; cin >> p.drK.iceTea; amount = amount + (1.15 * p.drK.iceTea); break;
+				} 
+				
+		break;
+		default: "Escoga una opcion corecta"; 
+	}
+	restaurantList.push_back(p);
 	
 	cout << "\nSu cuenta es: " << "$" << amount <<endl;
 		
